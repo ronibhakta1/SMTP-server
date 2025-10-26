422a9d9e9c4b57374af5ff5a30ebb07478b6c0fc
Roni bhakta <ronibhakta1@gmail.com>
2025-10-26 17:02:16 +0530

    feat: implement SMTP server event handlers and error logging

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const { SMTPServer } = require("smtp-server");
 
-const server = new SMTPServer(options);
+const server = new SMTPServer({
+    sesure: false,
+    onConnect(session, callback){
+        console.log("Client Connected", session.id);
+        callback();
+    },
+    onMailFrom(address, session, callback){
+        console.log("Mail From:", address.address, session.id);
+        callback();
+    },
+    onRcptTo(address,session,callback){
+        console.log("Recipient To:", address.address, session.id);
+        callback();
+    }
+    
+});
 
-server.listen(port[, host][, callback]);
+server.on("error", (err)=>{
+    console.log("SMTP Server Error Ocurred", err.message);
+})
+server.listen(port[22, host][port, callback]);
 
 server.close(callback);
